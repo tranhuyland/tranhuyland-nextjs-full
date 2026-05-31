798a0b4ae5b165e1d7c2478efa6d6c9e0be63ada
tranhuyland <huy07kt@gmail.com>
2026-05-31 20:43:20 +0700

    Add files via upload

---
+++ src/app/api/bds/route.js
@@ -0,0 +1,7 @@
+import { getBdsData } from "@/lib/googleSheets";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const data = await getBdsData();
+  return NextResponse.json(data);
+}

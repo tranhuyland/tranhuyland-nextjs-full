42dccec7a9f669518d382e3e0b4fb14f128c9229
tranhuyland <huy07kt@gmail.com>
2026-05-31 22:39:58 +0700

    Update sitemap.js

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,1 +1,24 @@
-
+export default function sitemap() {
+  return [
+    {
+      url: "https://tranhuyland.vn",
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: "https://tranhuyland.vn/quan-hai-chau",
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: "https://tranhuyland.vn/quan-cam-le",
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: "https://tranhuyland.vn/quan-son-tra",
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+  ];
+}

448ce6c3dc64e58f2d0df9dc7f4d6a83069eb93e
tranhuyland <huy07kt@gmail.com>
2026-06-01 00:22:16 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import { getBdsData } from "../lib/googleSheets";
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import ListingSection from "../components/ListingSection";
-import About from "../components/About";
-import Blog from "../components/Blog";
-import ContactCTA from "../components/ContactCTA";
-import Footer from "../components/Footer";
+import { getBdsData } from "@/lib/googleSheets";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import ListingSection from "@/components/ListingSection";
+import About from "@/components/About";
+import Blog from "@/components/Blog";
+import ContactCTA from "@/components/ContactCTA";
+import Footer from "@/components/Footer";
 
 export default async function Home() {
   // Fetch dữ liệu trực tiếp từ Server để tối ưu Google SEO

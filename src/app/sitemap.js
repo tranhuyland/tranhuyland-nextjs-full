923889b5dd41d8e8c74d6f57cb03a38eeebf8e95
tranhuyland <huy07kt@gmail.com>
2026-06-01 14:01:44 +0700

    Update sitemap.js

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,10 +1,12 @@
 import { getBdsData } from "@/lib/googleSheets";
 
+export const revalidate = 600;
+
 export default async function sitemap() {
   const data = await getBdsData();
 
-  const properties = data.map((item) => ({
-    url: `https://tranhuyland.vn/nha-dat/${item.id}`,
+  const routes = data.map((item) => ({
+    url: `https://tranhuyland.vn/nha-dat/${item.slug}`,
     lastModified: new Date(),
   }));
 
@@ -13,27 +15,6 @@ export default async function sitemap() {
       url: "https://tranhuyland.vn",
       lastModified: new Date(),
     },
-
-    {
-      url: "https://tranhuyland.vn/quan/hai-chau",
-      lastModified: new Date(),
-    },
-
-    {
-      url: "https://tranhuyland.vn/quan/cam-le",
-      lastModified: new Date(),
-    },
-
-    {
-      url: "https://tranhuyland.vn/quan/son-tra",
-      lastModified: new Date(),
-    },
-
-    {
-      url: "https://tranhuyland.vn/quan/ngu-hanh-son",
-      lastModified: new Date(),
-    },
-
-    ...properties,
+    ...routes,
   ];
 }
